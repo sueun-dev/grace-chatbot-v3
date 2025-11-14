1846ba89d748e6c055c914517f11b5fdf2ece5f7
root <root@inst-builder-debian-12-build-build-2ssr5.local>
2025-11-14 21:44:29 +0000

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    name: 'grace-chatbot',
+    script: 'npm',
+    args: 'start',
+    cwd: '/home/grace-chatbot-v2',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3001  // 3001번 포트로 설정
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    time: true,
+    max_restarts: 10
+  }]
+}
