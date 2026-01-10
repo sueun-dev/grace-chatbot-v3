79dae70f3eb7293e56aba7afb453cf2f4f90c1bc
sueun-dev <57546981+sueun-dev@users.noreply.github.com>
2026-01-09 18:09:37 -0600

    Center student sidebar artwork

--- app/[role]/components/StudentChatbot.jsx
+++ app/[role]/components/StudentChatbot.jsx
@@ -210,22 +210,24 @@ const Sidebar = ({ showSidebar }) => {
   return (
     <div
       className={clsx(
-        "absolute top-0 left-0 bottom-0 overflow-hidden translate-x-[-100%] lg:static lg:w-0 lg:max-w-0 transition-all duration-300 z-index-10 h-full rounded-[16px] border-[1px] border-[#F0F2F5] flex flex-col justify-between items-center box-shadow bg-[url('/student-bg.png')] bg-cover bg-center",
+        "absolute top-0 left-0 bottom-0 overflow-hidden translate-x-[-100%] lg:static lg:w-0 lg:max-w-0 transition-all duration-300 z-index-10 h-full rounded-[16px] border-[1px] border-[#F0F2F5] flex flex-col justify-center items-center box-shadow bg-[url('/student-bg.png')] bg-cover bg-center",
         showSidebar &&
           "translate-x-0 lg:translate-x-0 lg:w-[296px] lg:max-w-[296px] p-[17px]"
       )}
     >
-      <div className="self-start">
+      <div className="absolute top-[17px] left-[17px]">
         <Logo />
       </div>
-      <div className="h-[513px]">
-        <Image
-          src="/student-cartoon.png"
-          width={100}
-          height={100}
-          alt="logo"
-          className="w-full h-full object-cover"
-        />
+      <div className="flex flex-col justify-center items-center gap-[5px] max-w-[90%] mx-auto">
+        <div className="h-[480px]">
+          <Image
+            src="/student-cartoon.png"
+            width={100}
+            height={100}
+            alt="logo"
+            className="w-full h-full object-cover"
+          />
+        </div>
       </div>
     </div>
   );
